4d030c60cf7d9361b0812717fe220180f6a7edda
SAI SATHWIK REDDY VARIKOTI <varikotisaisathwikreddy@gmail.com>
2024-04-12 00:22:25 -0400

    fix too large thing

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app: Express = express();
 const port: number = 8081;
 export const jwtSecret: string = process.env.JWT_SECRET as string;
 
-
+app.use(express.json({ limit: '50mb' }));
 app.use(express.json());
 app.use(express.urlencoded({
 	extended: true,
